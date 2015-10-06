624a2c3740f8e89a6d80215d4073b8e5078a2fa2
Ben Batschelet <ben@codeup.com>
2015-10-06 17:51:57 -0500

    Test for bash completion, tests for console_user variable

--- spec/bash-console-user-spec.rb
+++ spec/bash-console-user-spec.rb
@@ -2,21 +2,11 @@ require_relative 'spec_helper'
 
 RSpec.configure do |config|
   config.before :suite do
-    SpecHelper.instance.provision 'playbooks/bash-profile-default.yml'
+    SpecHelper.instance.provision 'playbooks/bash-profile-user.yml'
   end
 end
 
-describe file('/etc/profile.d/bash_completion.sh') do
-  it { should exist }
-  it { should be_file }
-end
-
-# Can't think of a good way to test color prompt; the following doesn't work =(
-# describe command("bash -ilc 'echo $PS1'") do
-#   its(:stdout) { should include "\\[\\033[01;32m\\]\\u@\\h\\[\\033[00m\\]" }
-# end
-
-describe command("bash -ic 'll ~'") do
+describe command("sudo su - test_user -c \"bash -ic 'll ~'\"") do
   its(:stdout) { should match /^-rw-r--r--\s+\d+\s+\w+\s+\w+\s+1.0K\s+\w+\s+\d+\s+\d+(:\d+)?\s+test$/ }
   its(:stdout) { should match /^drwxr-xr-x\s+\d+\s+\w+\s+\w+\s+[0-9.]+(K|M|G)?\s+\w+\s+\d+\s+\d+(:\d+)?\s+test_dir\/$/ }
 
@@ -24,7 +14,7 @@ describe command("bash -ic 'll ~'") do
   its(:exit_status) { should eq 0 }
 end
 
-describe command("bash -ic 'la ~'") do
+describe command("sudo su - test_user -c \"bash -ic 'la ~'\"") do
   its(:stdout) { should match /^-rw-r--r--\s+\d+\s+\w+\s+\w+\s+1.0K\s+\w+\s+\d+\s+\d+(:\d+)?\s+test$/ }
   its(:stdout) { should match /^-rwxr-xr-x\s+\d+\s+\w+\s+\w+\s+1.0M\s+\w+\s+\d+\s+\d+(:\d+)?\s+.test\*$/ }
   its(:stdout) { should match /^drwxr-xr-x\s+\d+\s+\w+\s+\w+\s+[0-9.]+(K|M|G)?\s+\w+\s+\d+\s+\d+(:\d+)?\s+test_dir\/$/ }
