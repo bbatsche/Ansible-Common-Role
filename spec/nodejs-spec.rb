19a92082135d86f1287ada546bc7b28575e1e1af
Ben Batschelet <ben@codeup.com>
2015-10-08 11:42:23 -0500

    Nodejs spec

---
+++ spec/nodejs-spec.rb
@@ -0,0 +1,23 @@
+require_relative 'spec_helper'
+
+RSpec.configure do |config|
+  config.before :suite do
+    SpecHelper.instance.provision 'playbooks/nodejs.yml'
+  end
+end
+
+describe command("node -e \"console.log('node installed');\"") do
+  its(:stdout) { should eq "node installed\n" }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("nodejs -e \"console.log('node installed');\"") do
+  its(:stdout) { should eq "node installed\n" }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("npm version") do
+  its(:exit_status) { should eq 0 }
+end
+
+
