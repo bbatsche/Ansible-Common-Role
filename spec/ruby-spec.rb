b581d267c99b27afebd30dbf9daa9060eb0332db
Ben Batschelet <ben@codeup.com>
2015-10-08 12:04:37 -0500

    Ruby spec

---
+++ spec/ruby-spec.rb
@@ -0,0 +1,41 @@
+require_relative 'spec_helper'
+
+RSpec.configure do |config|
+  config.before :suite do
+    SpecHelper.instance.provision 'playbooks/ruby.yml'
+  end
+end
+
+describe command("ruby -e \"puts 'ruby installed'\"") do
+  its(:stdout) { should eq "ruby installed\n" }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("ruby2.0 -e \"puts 'ruby installed'\"") do
+  its(:stdout) { should eq "ruby installed\n" }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("ruby1.9.1 --version") do
+  its(:stdout) { should match /\b1\.9\.\d+/ }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("ruby2.0 --version") do
+  its(:stdout) { should match /\b2\.0\.\d+/ }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("gem --version") do
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("gem1.9.1 --version") do
+  its(:stdout) { should match /\b1\.\d+\.\d+/ }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("gem2.0 --version") do
+  its(:stdout) { should match /\b2\.\d+\.\d+/ }
+  its(:exit_status) { should eq 0 }
+end
