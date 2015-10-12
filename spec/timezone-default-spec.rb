606faf98b9d440fbc8cfd5b46a4ca8d3bf7e9c02
Ben Batschelet <ben@codeup.com>
2015-10-12 16:46:05 -0500

    Timezone specs

---
+++ spec/timezone-default-spec.rb
@@ -0,0 +1,11 @@
+require_relative 'spec_helper'
+
+RSpec.configure do |config|
+  config.before :suite do
+    SpecHelper.instance.provision 'playbooks/timezone.yml'
+  end
+end
+
+describe command("date '+%Z'") do
+  its(:stdout) { should match /UTC/ }
+end
