8722b6e4f87cb6ac971b3fc79762e1f3bf62fb07
Ben Batschelet <bbatschelet@socialsolutions.com>
2017-06-27 22:32:57 -0500

    Updated specs for ACLs

--- spec/acl-spec.rb
+++ spec/acl-spec.rb
@@ -1,9 +1,10 @@
+require "net/ssh"
 require_relative "lib/ansible_helper"
 require_relative "bootstrap"
 
 RSpec.configure do |config|
   config.before :suite do
-    AnsibleHelper.instance.playbook 'playbooks/acl.yml'
+    AnsibleHelper.instance.playbook "playbooks/acl.yml"
   end
 end
 
@@ -11,27 +12,26 @@ describe group("web-admin") do
   it { should exist }
 end
 
-describe user("vagrant") do
+describe user(AnsibleHelper.instance.sshOptions) do
   it { should belong_to_group "web-admin" }
 end
 
-describe command("setfacl -m g:web-admin:rwx,d:g:web-admin:rwx ~/acl_test") do
+describe command("setfacl -m g:web-admin:rwx,d:g:web-admin:rwx $HOME/acl_test") do
   let(:disable_sudo) { false }
-  it "has ACLs enabled" do
-    expect(subject.stderr).to eq ""
-    expect(subject.exit_status).to eq 0
+
+  describe "setting file ACL" do
+    include_examples "no errors"
   end
 end
 
-describe command("touch ~/acl_test/test") do
+describe command("touch $HOME/acl_test/test") do
   after :all do
     set :disable_sudo, false
-    raise "Cleanup Failed" if command("rm -rf /home/vagrant/acl_test").exit_status != 0
+    raise "Cleanup Failed" if command("rm -rf $HOME/acl_test").exit_status != 0
     set :disable_sudo, true
   end
 
-  it "allows the current user to write to the directory" do
-    expect(subject.stderr).to eq ""
-    expect(subject.exit_status).to eq 0
+  describe "using file ACLs" do
+    include_examples "no errors"
   end
 end
